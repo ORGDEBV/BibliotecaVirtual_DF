3c8c90252832f76609b2b62411913535c580e782
Renato Vásquez Tejada <renatovt11@gmail.com>
2016-08-31 11:07:17 -0500

    INTEGRACION

--- src/java/vb/util/ConectaDb.java
+++ src/java/vb/util/ConectaDb.java
@@ -16,11 +16,7 @@ public class ConectaDb {
             ResourceBundle props = ResourceBundle.getBundle("vb.util.bv");
             Class.forName(props.getString("DRIVER"));
 
-            //cn = DriverManager.getConnection("jdbc:sqlserver://DEV1BNP:1433;databaseName=BVIRTUAL", "devbvirtual", "devbvirtual");
-           //cn = DriverManager.getConnection("jdbc:sqlserver://100101060104-32:1433;databaseName=BVIRTUAL_V2", "sa", "123456789");
             cn = DriverManager.getConnection(props.getString("URL"),props.getString("USUARIO"),props.getString("PASSWORD"));
-           // cn = DriverManager.getConnection("jdbc:sqlserver://100101060104-29:1433;databaseName=BD_BVIRTUAL", "bvirtual", "123456789");
-            //cn = DriverManager.getConnection("jdbc:sqlserver://FEARLESS:1433;databaseName=BVIRTUAL", "sa", "123456789");
             if (cn != null) {
                 System.out.println("Conexion realizada satisfactoriamente.");
             }
