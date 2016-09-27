22172800746fd38defa65e812f1124077c9f289f
Ludwig <Ludwig.marusic.h@gmail.com>
2016-09-27 09:06:41 -0500

    cambios

--- src/java/bv/util/CambiarNombre.java
+++ src/java/bv/util/CambiarNombre.java
@@ -16,26 +16,35 @@ public class CambiarNombre {
 
     /**
      * @param args the command line arguments
-     */
+     */ 
     public static void main(String[] args) {
-        String hola = "                    HOLA K ASE   a              ";
-        System.out.println(hola.trim()+"l");
-//        sql cn = new sql();
-//        List<Object[]> list = cn.consulta("SELECT OTRO,ID_DOCUMENTAL FROM BV.DOCUMENTAL");
-//        for (Object[] data : list) {
-//            File f1 = new File("Z:\\ehe\\" + data[0]);
-//            File f2 = new File("Z:\\ehe\\" + data[1].toString());
-//            if (f1.exists()) {
-//                boolean correcto = f1.renameTo(f2);
-//                if (correcto) {
-//                    System.out.println("Se cambió nombre del archivo: " + data[0]);
-//                } else {
-//                    System.out.println("Ocurrió un error al cambiar el nombre: " + data[0] + " por el nombre: " + data[1]);
-//                }
-//            } else {
-//                System.out.println("No se encontro la carpeta con el nombre: " + data[0]);
+
+//        File dir = new File("Z:\\virtual");
+//        String[] ficheros = dir.list();
+//        if (ficheros == null) {
+//            System.out.println("No hay ficheros en el directorio especificado");
+//        } else {
+//            for (int x = 0; x < ficheros.length; x++) {
+//                System.out.println(ficheros[x]);
 //            }
 //        }
+
+        sql cn = new sql();
+        List<Object[]> list = cn.consulta("SELECT ID_DOCUMENTAL, OTRO FROM BV.DOCUMENTAL WHERE ACTIVO=1");
+        for (Object[] data : list) {
+            File f1 = new File("Z:\\virtual\\" + data[0]);
+            File f2 = new File("Z:\\virtual\\" + data[1].toString());
+            if (f1.exists()) {
+                boolean correcto = f1.renameTo(f2);
+                if (correcto) {
+                    System.out.println("Se cambió nombre del archivo: " + data[0]);
+                } else {
+                    System.out.println("Ocurrió un error al cambiar el nombre: " + data[0] + " por el nombre: " + data[1]);
+                }
+            } else {
+                System.out.println("No se encontro la carpeta con el nombre: " + data[0]);
+            }
+        }
     }
 
 }
